196ca2d75448587dcd68101c2d8d0918d632fb71
lautaro <lautaro@sanluis.fierro.com.ar>
2016-06-07 11:45:36 -0300

    chore(release): Release v3.1.2

--- src/tags-input.js
+++ src/tags-input.js
@@ -402,10 +402,6 @@ tagsInput.directive('tagsInput', function($timeout, $document, $window, $q, tags
                     ngModelCtrl.$setValidity('leftoverText', true);
                 })
                 .on('input-blur', function() {
-                    if (scope.text[scope.newTag.text().length - 1] === ',') {
-                        scope.newTag.text(scope.newTag.text().split(',')[0]);
-                    }
-
                     if (options.addOnBlur && !options.addFromAutocompleteOnly) {
                         tagList.addText(scope.newTag.text());
                     }
@@ -417,9 +413,10 @@ tagsInput.directive('tagsInput', function($timeout, $document, $window, $q, tags
                         addKeys = {},
                         shouldAdd, shouldRemove, shouldSelect, shouldEditLastTag;
 
+                  alert(event.keyCode);
+                  alert(event.keyChar);
                     if (scope.text[scope.newTag.text().length - 1] === ',') {
                         key = KEYS.comma;
-                        scope.newTag.text(scope.newTag.text().split(',')[0]);
                     }
 
                     if (tiUtil.isModifierOn(event) || hotkeys.indexOf(key) === -1) {
